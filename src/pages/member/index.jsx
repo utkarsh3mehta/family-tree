69ba0b9680796e64f7e62edaa2df0743f665b872
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-11-02 02:14:24 +0530

    added member page

---
+++ src/pages/member/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { FindChildren, FindFamily } from "../../family";
+import { Navbar } from "../../atoms/navbar";
+import { SingleMember } from "../../molecules/single-member";
+import styles from "./member.module.css";
+
+export const Member = (props) => {
+  const [member, setMember] = useState({});
+
+  useEffect(() => {
+    setMember(FindFamily(props.match.params.memuid));
+  }, [props.match.params.memuid]);
+
+  return (
+    <div className="d-flex column start align-center relative">
+      <Navbar member={member} memuid={props.match.params.memuid} />
+      <h2 className={`self-align-end col-8 text-right m-half ${styles.name}`}>
+        {"name" in member ? member["name"] : "loading..."}
+      </h2>
+      <h1 className="mt-half">Children</h1>
+      <div className="d-flex column r-g-1 py-1">
+        {FindChildren(props.match.params.memuid).map((child) => {
+          return (
+            <SingleMember key={child["uid"]} uid={child["uid"]} single={true} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
