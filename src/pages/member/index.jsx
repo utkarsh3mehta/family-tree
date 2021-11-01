8de2018cd671bd645c379f25f90a02557abdea38
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-11-02 02:45:51 +0530

    responsive member page

--- src/pages/member/index.jsx
+++ src/pages/member/index.jsx
@@ -14,11 +14,11 @@ export const Member = (props) => {
   return (
     <div className="d-flex column start align-center relative">
       <Navbar member={member} memuid={props.match.params.memuid} />
-      <h2 className={`self-align-end col-8 text-right m-half ${styles.name}`}>
+      <h2 className={`self-align-end col-8 col-md-4 text-right m-half ${styles.name}`}>
         {"name" in member ? member["name"] : "loading..."}
       </h2>
       <h1 className="mt-half">Children</h1>
-      <div className="d-flex column r-g-1 py-1">
+      <div className="d-flex column center md-row wrap col-8 margin-center r-g-1 c-g-2 py-1">
         {FindChildren(props.match.params.memuid).map((child) => {
           return (
             <SingleMember key={child["uid"]} uid={child["uid"]} single={true} />
