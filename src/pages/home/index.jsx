54cca2d4cf53ef05618ecb59d277613b4e0a9c6d
Utkarsh Mehta <utkarshmehta143@gmail.com>
2021-11-02 02:14:32 +0530

    added home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SingleMember } from "../../molecules/single-member";
+import styles from './home.module.css';
+
+export const Home = (props) => {
+  return (
+    <div className={`d-flex column center align-center r-g-1 ${styles.container}`}>
+      <h1 className="text-center col-8">Welcome to Mehta Family tree</h1>
+      <SingleMember uid="spm-19xx" />
+    </div>
+  );
+};
