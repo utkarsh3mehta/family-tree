124d3856c89b754cffbc81ceef217e70a817e044
Utkarsh Mehta <utkarshmehta143@gmail.com>
2022-01-21 00:02:38 +0530

    login better design

--- src/templates/login/index.jsx
+++ src/templates/login/index.jsx
@@ -27,30 +27,24 @@ export const Login = (props) => {
       onSubmit={onSubmit}
       className={`d-flex column align-center r-g-1 col-12 px-2 ${styles.form}`}
     >
-      <div>
-        <label htmlFor="username">
-          Username
-        </label>
-        <input
-          value={username}
-          name="username"
-          type="text"
-          onChange={onChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password
-        </label>
-        <input
-          value={password}
-          name="password"
-          type="password"
-          onChange={onChange}
-          required
-        />
-      </div>
+      <input
+        value={username}
+        name="username"
+        placeholder="Username"
+        type="text"
+        onChange={onChange}
+        required
+        autoComplete="current-username"
+      />
+      <input
+        value={password}
+        name="password"
+        placeholder="Password"
+        type="password"
+        onChange={onChange}
+        required
+        autoComplete="current-password"
+      />
       <input className="p-1" type="submit" value="Login" />
       {error && (
         <span className={`${styles.error}`}>Invalid username or password</span>
